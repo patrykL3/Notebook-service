03d1ce3b28218810545738e17219cd601ee38038
Patryk Łubik <patryk.lubik@wp.pl>
2021-05-03 10:42:02 +0200

    skeleton

---
+++ src/test/java/pl/patryklubik/Notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patryklubik.Notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
