deda252c5d1dded576dcc176fdb6fb29d05090b2
Patryk Łubik <patryk.lubik@wp.pl>
2021-06-06 12:15:50 +0200

    add primary key MSSQL

--- src/main/resources/db/migration/V1__init_notes_table_MSSQL.sql
+++ src/main/resources/db/migration/V1__init_notes_table_MSSQL.sql
@@ -1,6 +1,6 @@
 drop table if exists notes;
 create table notes(
-    id int identity(1,1),
+    id int identity(1,1) primary key,
     title varchar(100) not null,
     description varchar(100) not null,
     importance_flag bit
