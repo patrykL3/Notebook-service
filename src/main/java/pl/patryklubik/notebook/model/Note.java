dc3cef346c4393cc89908f59afa9e0531384f5f8
Patryk Łubik <patryk.lubik@wp.pl>
2021-08-13 18:25:47 +0200

    lombok

--- src/main/java/pl/patryklubik/notebook/model/Note.java
+++ src/main/java/pl/patryklubik/notebook/model/Note.java
@@ -1,5 +1,8 @@
 package pl.patryklubik.notebook.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
@@ -8,6 +11,8 @@ import javax.validation.constraints.NotBlank;
  * Create by Patryk Łubik on 03.05.2021.
  */
 
+@Getter
+@Setter
 @Entity
 @Table(name = "notes")
 public class Note {
@@ -23,37 +28,6 @@ public class Note {
 
     Note() {}
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isImportanceFlag() {
-        return importanceFlag;
-    }
-
-    public void setImportanceFlag(boolean importanceFlag) {
-        this.importanceFlag = importanceFlag;
-    }
 
     public void updateFrom(final Note source) {
 
